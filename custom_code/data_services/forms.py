78eb09fcecaf59c608b4562ba92420a61a5184da
joysankar-astro <joysanka@bhu.ac.in>
2026-03-12 13:14:36 +0100

    Fix SkyMapper QueryForm

--- custom_code/data_services/forms.py
+++ custom_code/data_services/forms.py
@@ -84,7 +84,7 @@ class SkyMapperQueryForm(BaseQueryForm):
         has_coords = cleaned.get('ra') is not None and cleaned.get('dec') is not None
         if not has_coords:
             raise forms.ValidationError('Provide RA+Dec.')
-        if cleaned.get('radius_arcmin') is None:
-            cleaned['radius_arcmin'] = 0.1
+        if cleaned.get('radius_arcsec') is None:
+            cleaned['radius_arcsec'] = 5.0
         return cleaned
 
