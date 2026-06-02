c938cfcffce9045615b1419ca889477b15f86fec
Łukasz Wyrzykowski <wyrzykow@gmail.com>
2026-06-02 18:44:49 +0200

    Add merge migration for custom_code branches

---
+++ custom_code/migrations/0014_merge_custom_code_branches.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('custom_code', '0006_facility_facilityaccount_facilityaccountmembership_and_more'),
+        ('custom_code', '0013_userbhtom2uploadpreference'),
+    ]
+
+    operations = []
