1d7344e4ebc9944e0608c05ccedf81f612d49ece
Łukasz Wyrzykowski <wyrzykow@gmail.com>
2026-07-15 13:55:09 +0200

    Add test for non-sidereal target detail and update template to handle computed coordinates

--- custom_code/tests.py
+++ custom_code/tests.py
@@ -2493,6 +2493,41 @@ class TargetDetailDataTests(TestCase):
         self.assertEqual(kwargs['observer_elevation_m'], 120.0)
         self.assertEqual(kwargs['time_to_compute'].to_datetime(timezone=timezone.utc).isoformat(), '2026-04-08T12:34:56+00:00')
 
+    def test_non_sidereal_target_detail_displays_zero_computed_coordinates(self):
+        target = Target.objects.create(
+            name='MinorPlanetZero',
+            type=Target.NON_SIDEREAL,
+            scheme='MPC_MINOR_PLANET',
+            semimajor_axis=2.35,
+            eccentricity=0.17,
+            inclination=8.4,
+            arg_of_perihelion=132.5,
+            lng_asc_node=76.2,
+            mean_anomaly=48.1,
+            epoch_of_elements=61000.0,
+            mean_daily_motion=0.274,
+        )
+
+        with patch('custom_code.templatetags.live_target_extras.get_live_target_values', return_value={
+            'ra': 0.0,
+            'dec': 0.0,
+            'sun_separation': 101.0,
+            'altitude_deg': 45.0,
+            'computed_at_utc': datetime(2026, 4, 8, 12, 34, 56, tzinfo=timezone.utc),
+        }):
+            response = self.client.get(
+                reverse('targets:detail', kwargs={'pk': target.pk}),
+                {
+                    'observer': 'ostrowik',
+                    'time_utc': '2026-04-08T12:34:56',
+                },
+            )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Computed Ra,Dec')
+        self.assertContains(response, '0.0 0.0')
+        self.assertContains(response, 'Compute current Ra/Dec')
+
 
 class GaiaCurrentCoordinateComputationTests(TestCase):
     def test_can_compute_current_coordinates_requires_good_parallax_signal_to_noise(self):
